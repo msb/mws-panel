613e80bda4ad22412c97c9740739c83f680769f8
Mike Bamford <mb2174@cam.ac.uk>
2018-01-16 13:47:28 +0000

    first stab at config

--- mws/mws/wsgi.py
+++ mws/mws/wsgi.py
@@ -4,11 +4,11 @@ WSGI config for MWS project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mws.production_settings")
 
 from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
